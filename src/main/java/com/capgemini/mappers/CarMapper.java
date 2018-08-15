1332b144c36a9a52e7ae3e9eb49a044502bf1340
TL <31960565+kredki@users.noreply.github.com>
2018-08-15 20:56:55 +0200

    add car service

--- src/main/java/com/capgemini/mappers/CarMapper.java
+++ src/main/java/com/capgemini/mappers/CarMapper.java
@@ -7,6 +7,7 @@ import com.capgemini.types.CarTO;
 import com.capgemini.types.EmployeeTO;
 import com.capgemini.types.RentalTO;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -41,5 +42,13 @@ public class CarMapper {
 
     public static Set<CarEntity> map2Entities (Set<CarTO> cars) {
         return cars.stream().map(CarMapper::toEntity).collect(Collectors.toSet());
+    }
+
+    public static List<CarTO> map2TOs (List<CarEntity> cars) {
+        return cars.stream().map(CarMapper::toTO).collect(Collectors.toList());
+    }
+
+    public static List<CarEntity> map2Entities (List<CarTO> cars) {
+        return cars.stream().map(CarMapper::toEntity).collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/capgemini/service/impl/CarServiceImpl.java
@@ -0,0 +1,33 @@
+package com.capgemini.service.impl;
+
+import com.capgemini.dao.CarDao;
+import com.capgemini.domain.CarEntity;
+import com.capgemini.mappers.CarMapper;
+import com.capgemini.service.CarService;
+import com.capgemini.types.CarTO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+    @Autowired
+    CarDao carRepository;
+
+    @Override
+    public List<CarTO> findCarByType(String type) {
+        List<CarEntity> cars = carRepository.findCarByType(type);
+        return CarMapper.map2TOs(cars);
+    }
+
+    @Override
+    public List<CarTO> findCarByBrand(String brand) {
+        List<CarEntity> cars = carRepository.findCarByBrand(brand);
+        return CarMapper.map2TOs(cars);
+    }
+
+    @Override
+    public List<CarTO> findCarByCaretaker(long caretakerId) {
+        List<CarEntity> cars = carRepository.findCarByCaretaker(caretakerId);
+        return CarMapper.map2TOs(cars);
+    }
+}
