9fa37b50aff51d7e6b0dbc1f32b7247ed22c5f0b
TL <31960565+kredki@users.noreply.github.com>
2018-08-17 08:35:19 +0200

    add sql logging

--- src/test/java/com/capgemini/service/CarServiceTest.java
+++ src/test/java/com/capgemini/service/CarServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
@@ -23,8 +22,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@PropertySource("application-hsql.properties")
+@SpringBootTest(properties = "spring.profiles.active=hsql")
 @Transactional
 public class CarServiceTest {
     @Autowired
